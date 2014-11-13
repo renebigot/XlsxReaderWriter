050360b698f91705b5290af451481d648d33af39
René BIGOT <renebigot@MacBook-Pro-de-Rene.local>
2014-11-13 08:04:58 +0100

    Folder initialisation

---
+++ XlsxReaderWriter/XlsxReaderWriter.h
@@ -0,0 +1,13 @@
+//
+//  XlsxReaderWriter.h
+//  XlsxReaderWriter
+//
+//  Created by René BIGOT on 13/11/2014.
+//  Copyright (c) 2014 BRAE. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface XlsxReaderWriter : NSObject
+
+@end
---
+++ XlsxReaderWriter/XlsxReaderWriter.m
@@ -0,0 +1,13 @@
+//
+//  XlsxReaderWriter.m
+//  XlsxReaderWriter
+//
+//  Created by René BIGOT on 13/11/2014.
+//  Copyright (c) 2014 BRAE. All rights reserved.
+//
+
+#import "XlsxReaderWriter.h"
+
+@implementation XlsxReaderWriter
+
+@end
