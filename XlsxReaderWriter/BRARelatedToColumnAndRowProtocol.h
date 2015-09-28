acd916234d10c860208dfd167aa4ea031b4f0261
René Bigot <bigotrene@gmail.com>
2015-09-28 07:59:56 +0200

    Add BRARelatedToColumnAndRowProtocol.h

---
+++ XlsxReaderWriter/BRARelatedToColumnAndRowProtocol.h
@@ -0,0 +1,19 @@
+//
+//  BRARelatedToColumnAndRowProtocol.h
+//  XlsxReaderWriter
+//
+//  Created by René BIGOT on 23/09/2015.
+//  Copyright © 2015 BRAE. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol BRARelatedToColumnAndRowProtocol <NSObject>
+
+@required
+- (void)didAddRowsAtIndexes:(NSIndexSet *)indexes;
+- (void)didRemoveRowsAtIndexes:(NSIndexSet *)indexes;
+- (void)didAddColumnsAtIndexes:(NSIndexSet *)indexes;
+- (void)didRemoveColumnsAtIndexes:(NSIndexSet *)indexes;
+
+@end
